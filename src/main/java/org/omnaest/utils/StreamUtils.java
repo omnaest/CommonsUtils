0e94bd175c76798404aad0d283bb321dc9437b96
omnaest <awonderland6@googlemail.com>
2017-08-27 11:42:58 +0200

    added StreamUtils

---
+++ src/main/java/org/omnaest/utils/StreamUtils.java
@@ -0,0 +1,69 @@
+/*
+
+	Copyright 2017 Danny Kunz
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+
+*/
+package org.omnaest.utils;
+
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamUtils
+{
+	public static <E> Stream<E> concat(Stream<Stream<E>> streams)
+	{
+		return streams	.reduce(Stream::concat)
+						.orElseGet(() -> Stream.empty());
+	}
+
+	public static <E> Stream<E> fromIterator(Iterator<E> iterator)
+	{
+		return StreamSupport.stream(((Iterable<E>) () -> iterator).spliterator(), false);
+	}
+
+	public static <E> Stream<E> fromSupplier(Supplier<E> supplier, Predicate<E> terminateMatcher)
+	{
+		return fromIterator(new Iterator<E>()
+		{
+			private AtomicReference<E> takenElement = new AtomicReference<>();
+
+			@Override
+			public boolean hasNext()
+			{
+				this.takeOneElement();
+				return terminateMatcher	.negate()
+										.test(this.takenElement.get());
+			}
+
+			@Override
+			public E next()
+			{
+				this.takeOneElement();
+				return this.takenElement.getAndSet(null);
+			}
+
+			private void takeOneElement()
+			{
+				this.takenElement.getAndUpdate(e -> e != null ? e : supplier.get());
+			}
+		}).filter(terminateMatcher.negate());
+
+	}
+}

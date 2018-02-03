86ce5405911f7a041d5fcb29b51df739eedab473
Omnaest <omnaest@web.de>
2018-02-03 19:22:10 +0100

    improved CacheUtils

--- src/main/java/org/omnaest/utils/CacheUtils.java
+++ src/main/java/org/omnaest/utils/CacheUtils.java
@@ -19,11 +19,13 @@
 package org.omnaest.utils;
 
 import java.io.File;
+import java.util.function.Supplier;
 
 import org.omnaest.utils.cache.Cache;
 import org.omnaest.utils.cache.ConcurrentHashMapCache;
 import org.omnaest.utils.cache.JsonFolderFilesCache;
 import org.omnaest.utils.cache.JsonSingleFileCache;
+import org.omnaest.utils.element.cached.CachedElement;
 
 /**
  * @see Cache
@@ -31,35 +33,64 @@ import org.omnaest.utils.cache.JsonSingleFileCache;
  */
 public class CacheUtils
 {
-	/**
-	 * Reads the content of the source {@link Cache} and populates the content into the target {@link Cache}
-	 *
-	 * @param cacheSource
-	 * @param cacheTarget
-	 */
-	public static <V> void populateCacheContentToNewCache(Cache cacheSource, Cache cacheTarget)
-	{
-		if (cacheSource != null && cacheTarget != null)
-		{
-			for (String key : cacheSource.keySet())
-			{
-				cacheTarget.put(key, cacheSource.get(key, cacheSource.getType(key)));
-			}
-		}
-	}
+    /**
+     * Reads the content of the source {@link Cache} and populates the content into the target {@link Cache}
+     *
+     * @param cacheSource
+     * @param cacheTarget
+     */
+    public static <V> void populateCacheContentToNewCache(Cache cacheSource, Cache cacheTarget)
+    {
+        if (cacheSource != null && cacheTarget != null)
+        {
+            for (String key : cacheSource.keySet())
+            {
+                cacheTarget.put(key, cacheSource.get(key, cacheSource.getType(key)));
+            }
+        }
+    }
 
-	public static Cache newConcurrentInMemoryCache()
-	{
-		return new ConcurrentHashMapCache();
-	}
+    public static Cache newConcurrentInMemoryCache()
+    {
+        return new ConcurrentHashMapCache();
+    }
 
-	public static <V> Cache newJsonFileCache(File cacheFile)
-	{
-		return new JsonSingleFileCache(cacheFile);
-	}
+    public static <V> Cache newJsonFileCache(File cacheFile)
+    {
+        return new JsonSingleFileCache(cacheFile);
+    }
 
-	public static <V> Cache newJsonFolderCache(File cacheDirectory)
-	{
-		return new JsonFolderFilesCache(cacheDirectory);
-	}
+    public static <V> Cache newJsonFolderCache(File cacheDirectory)
+    {
+        return new JsonFolderFilesCache(cacheDirectory);
+    }
+
+    /**
+     * Returns a {@link CachedElement} around the given {@link Supplier} using a json based {@link File} cache
+     * 
+     * @param type
+     * @param cacheFile
+     * @param supplier
+     * @return
+     */
+    public static <V> CachedElement<V> toFileCachedElement(Class<V> type, File cacheFile, Supplier<V> supplier)
+    {
+        boolean pretty = true;
+        return toFileCachedElement(type, cacheFile, pretty, supplier);
+    }
+
+    /**
+     * Similar to {@link #toFileCachedElement(Class, File, Supplier)} but allows to specify if pretty print is enabled or not
+     * 
+     * @param type
+     * @param cacheFile
+     * @param pretty
+     * @param supplier
+     * @return
+     */
+    public static <V> CachedElement<V> toFileCachedElement(Class<? super V> type, File cacheFile, boolean pretty, Supplier<V> supplier)
+    {
+        return CachedElement.of(supplier)
+                            .withFileCache(cacheFile, JSONHelper.writerSerializer(type), JSONHelper.readerDeserializer(type));
+    }
 }
